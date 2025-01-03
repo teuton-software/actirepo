71db39eb72c6f0a85622a7cff20de880c2cb6468
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2025-01-03 04:33:27 +0000

    Update __init__.py

--- src/actirepo/__init__.py
+++ src/actirepo/__init__.py
@@ -3,5 +3,5 @@ __module__ = 'actirepo'
 __icons_url__ = "https://raw.githubusercontent.com/teuton-software/actirepo/master/icons"
 __project_name__ = "actirepo"
 __project_version__ = "0.2.0"
-__project_description__ = "Organizador de cuestionarios Moodle en formato XML.'"
+__project_description__ = "Organizador de cuestionarios Moodle en formato XML."
 __project_url__ = "https://github.com/teuton-software/actirepo"
